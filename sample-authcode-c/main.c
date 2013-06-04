aec48e1b7dc79ff96b633d1d79bf5ea848791560
John Jelinek IV <jjelinek@tradestation.com>
2013-06-04 10:09:15 -0500

    Initial Commit

---
+++ sample-authcode-c/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  sample-authcode-c
+//
+//  Created by Administrator on 6/4/13.
+//  Copyright (c) 2013 TradeStation Technologies. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
